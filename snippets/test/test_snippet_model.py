bef400d9fc824b1e38ea65a245250d9d883db95b
phongnt1zigexn <phongnt1@zigexn.vn>
2024-09-26 14:58:36 +0700

    refactor to funtion-based

--- snippets/test/test_snippet_model.py
+++ snippets/test/test_snippet_model.py
@@ -1,30 +1,38 @@
 import pytest
 from django.urls import reverse
 from rest_framework import status
-from rest_framework.test import APITestCase
 from snippets.models import Snippet
-from rest_framework.test import APIClient
 from django.contrib.auth.models import User
+from rest_framework.test import APIClient
+
+@pytest.fixture
+def create_user(db):
+    user = User.objects.create_user(username='testuser', password='testpassword')
+    yield user
+    user.delete()
+
+@pytest.fixture
+def api_client():
+    return APIClient()
 
 def create_new_snippet(client):
-    url = '/snippets/'
+    url = '/snippets/' # Use reverse for URL resolution
     data = {'title': 'Test snippet', 'code': 'print("Hello, World!")', 'language': 'python'}
     return client.post(url, data, format='json')
 
-class TestSnippets(APITestCase):
-    def test_get_snippet_list(self):
-        url = '/snippets/'
-        response = self.client.get(url, format='json')
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+@pytest.mark.django_db
+def test_get_snippet_list(api_client):
+    url = '/snippets/'
+    response = api_client.get(url, format='json')
+    assert response.status_code == status.HTTP_200_OK
 
-    @pytest.mark.django_db
-    def test_create_snippet_without_auth(self):
-        response = create_new_snippet(self.client)
-        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+@pytest.mark.django_db
+def test_create_snippet_without_auth(api_client):
+    response = create_new_snippet(api_client)
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED
 
-    @pytest.mark.django_db
-    def test_create_snippet_with_basic_auth(self):
-        user = User.objects.create_user(username='testuser', password='testpassword')
-        self.client.force_authenticate(user=user)
-        response = create_new_snippet(self.client)
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+@pytest.mark.django_db
+def test_create_snippet_with_basic_auth(api_client, create_user):
+    api_client.force_authenticate(user=create_user)
+    response = create_new_snippet(api_client)
+    assert response.status_code == status.HTTP_201_CREATED
